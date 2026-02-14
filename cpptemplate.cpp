8e17d21053d4054292cb80ae82a4fe02bc212a1f
Kenneth Chen <92031860+kenJPG@users.noreply.github.com>
2026-02-14 18:09:37 +0800

    Update cpptemplate.cpp

--- cpptemplate.cpp
+++ cpptemplate.cpp
@@ -1,13 +1,46 @@
-#pragma GCC optimize("O3")
-#pragma GCC target("popcnt")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <bits/stdc++.h>
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define all(x) x.begin(), x.end()
-#define chmax(x, a) x = max(x, a);
-#define chmin(x, a) x = min(x, a);
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+
+template<typename T> bool chmin(T& a, const T& b) {
+    if (b < a) { a = b; return true; }
+    return false;
+}
+template<typename T> bool chmax(T& a, const T& b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
+
+void solve() {
+    
+}
 
 int main() {
     FASTIO;
-    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
